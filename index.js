5803a8b6dc47cd29153e20d9833221b6116d7b24
LiubovKvasova <rocket111185@gmail.com>
2024-07-13 00:04:47 +0300

    Now it's possible to use it as library

--- index.js
+++ index.js
@@ -2,23 +2,24 @@ import * as fs from 'fs/promises';
 import { compile } from './src/nfml.js';
 import { HELP_MODE, ARGUMENT_MAP, MODE_MAP, HELP_TEXT } from './constants.js';
 
-const parsedArguments = {
-    platform: 'HTML'
-};
-
 const fileSystemOptions = {
     encoding: 'utf8'
 };
 
-let argumentMode = '';
-let foundHelpArgument = false;
+const parseArguments = (...inputArguments) => {
+    const parsedArguments = {
+        platform: 'HTML',
+        foundHelpArgument: false
+    };
 
-export const main = async () => {
-    // Ignore first two arguments, since they are "node" and "index.js"
-    const inputArguments = process.argv.slice(2);
+    let argumentMode = '';
 
     for (const argument of inputArguments) {
         if (argumentMode) {
+            if (ARGUMENT_MAP[argument]) {
+                throw new Error(`Parameter ${argument} is not expectable`);
+            }
+
             const argumentProperty = MODE_MAP[argumentMode];
             parsedArguments[argumentProperty] = argument;
             argumentMode = '';
@@ -28,34 +29,60 @@ export const main = async () => {
             if (!argumentMode) {
                 throw new Error(`Argument ${argument} is not expectable`);
             } else if (argumentMode === HELP_MODE) {
-                foundHelpArgument = true;
+                parsedArguments.foundHelpArgument = true;
                 break;
             }
         }
     }
 
-    if (foundHelpArgument || !parsedArguments.filename) {
-        console.log(HELP_TEXT);
-        return;
-    }
+    return parsedArguments;
+};
 
-    const { filename, platform, outputFile } = parsedArguments;
+export const processNfml = async ({
+    filename,
+    platform,
+    outputFile
+}) => {
+    const result = {
+        error: false
+    };
+
     const input = await fs.readFile(filename, fileSystemOptions);
-
     const { error, compiledOutput } = await compile(input, platform);
 
     if (error) {
-        console.error(`${filename} contains errors. Please, see above.`);
-        process.exitCode = 1;
+        result.error = true;
+        result.message = `${filename} contains errors. Please, see above.`;
     } else if (outputFile) {
         await fs.writeFile(outputFile, compiledOutput, fileSystemOptions);
-        console.log(`The result has been written to file ${outputFile}`);
+        result.message = `The result has been written to file ${outputFile}`;
     } else {
-        console.log(compiledOutput);
+        result.output = compiledOutput;
     }
-}
+
+    return result;
+};
 
 // If this is loaded as a main file
 if (import.meta.url.endsWith(process.argv[1])) {
-    main();
+    // Ignore first two arguments, since they are "node" and "index.js"
+    const inputArguments = process.argv.slice(2);
+
+    const parsedArguments = parseArguments(...inputArguments);
+    const { foundHelpArgument, filename } = parsedArguments;
+
+    if (foundHelpArgument || !filename) {
+        console.log(HELP_TEXT);
+    } else {
+        const result = await processNfml(parsedArguments);
+
+        if (result.error) {
+            console.error(result.message);
+            process.exitCode = 1;
+        } else if (result.message) {
+            console.log(result.message);
+        } else if (result.output) {
+            console.log(result.output);
+        }
+    }
 }
